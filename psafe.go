cf4825b5d6da6d6dae7a7ac9fb6b873d21466bb9
Andrew Howden <hello@andrewhowden.com>
2016-11-08 21:54:46 +0100

    Playing with command line arguments

--- psafe.go
+++ psafe.go
@@ -3,21 +3,42 @@ package main
 import "fmt"
 import "os"
 
+/**
+ * Program entrypoint.
+ */
 func main() {
-	var args = parseArgs()
+	var args, isValid = parseArgs()
+	result := "Args are valid"
 
-	fmt.Printf(args)
+	if isValid == false {
+		result = "Args are not valid"
+	}
+
+	fmt.Printf(args + "\n")
+	fmt.Printf(result + "\n")
 }
 
-func parseArgs() string {
+/**
+ * Take the command line arguments, and return a structure that the rest of the program can work with. Responsible for
+ * handling any errors, such as no arguments or malformed arguments.
+ */
+func parseArgs() (string, bool) {
 	const defaultArg string = "help"
-	var arg string
+
+	allowedArgs := []string{"help", "ls", "find", "show", "grep", "insert", "edit", "generate", "rm", "mv", "cp", "version"}
+	arg := "help"
+	isValid := false
 
 	if len(os.Args) > 1 {
 		arg = os.Args[1]
-	} else {
-		arg = defaultArg
 	}
 
-	return arg
+	// Determine if the argument is even allowed.
+	for _, compare := range allowedArgs {
+		if compare == arg {
+			isValid = true
+		}
+	}
+
+	return arg, isValid
 }
